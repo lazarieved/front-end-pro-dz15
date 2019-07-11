8d1509bebf977bed0cc8665c03e745187e919f53
LazarievEd <lazarev223@gmail.com>
2019-07-12 02:12:26 +0300

    add dz15

---
+++ script.js
@@ -0,0 +1,17 @@
+function getNumberOfEven(n){
+    n = '' + n;
+    var sum = 0;
+    for (var j = 0; j < n.length; j++) {
+        if (n.charAt(j) % 2 === 0) {
+            sum++;
+        }
+    }
+    alert('Кол-во четных цыфр: ' + sum); 
+}
+
+getNumberOfEven(223344) // 4
+getNumberOfEven(111) // 0
+getNumberOfEven(123456) // 3
+getNumberOfEven(97531) // 0
+getNumberOfEven(8642) // 4
+getNumberOfEven(24688642) // 8
